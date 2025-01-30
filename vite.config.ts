674bb6bbd4ae46035011f2be614e153611d3a286
Ron Park <ronpark.dev@gmail.com>
2025-01-30 11:24:00 +0900

    chore: Add deploy workflow

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from "vite";
 import { resolve } from "path";
 
 export default defineConfig({
-  base: "./",
+  base: process.env.BASE_URL || "/",
   resolve: {
     alias: {
       "@": resolve(__dirname, "src"),
